90fcf26afc7e870d28c2771459c99fb4ad87d405
shyambhongle <shyambhongle@gmail.com>
2026-05-28 08:04:54 +0530

    Mobile UI fix

--- app/[locale]/docs/layout.tsx
+++ app/[locale]/docs/layout.tsx
@@ -7,7 +7,7 @@ export default function DocsLayout({ children }: { children: React.ReactNode }) 
       <Header />
       <div className="flex-1 flex">
         <Sidebar />
-        <main className="flex-1 px-4 md:px-8 py-8">{children}</main>
+        <main className="flex-1 min-w-0 overflow-x-hidden px-4 md:px-8 py-8">{children}</main>
         <MobileSidebar />
       </div>
     </div>
